58226fe6ca9c748b1d197af6eb7f3683699c3bf6
Poonam-dew <dewanganpoonam778@gmail.com>
2024-08-14 19:09:38 +0530

    addv user and video model

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ dotenv.config ({
   path:'./env'
 })
 connectDB()
-
+.then(()=> {
+  app.on("error",(error) => {
+    console.log("error on app",error)
+    throw error
+  })
+  app.listen(process.env.PORT || 8000 , () => {
+    console.log(` Server is running at port: ${process.env.PORT}`);
+    
+  })
+})
+.catch((err) =>{
+  console.log("MONGODB Connection Error !!!:",err)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next)=> {
+        Promise.resolve(requestHandler(req,res,next).
+    catch((err) => (err))).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+
+//const asyncHandler = ()=> {}
+//const asyncHandler = (func) =>{() =>{}}
+//const asyncHandler = (func) => () => {}
+//const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler=(fn)=>  async(req,res,next) => {
+//     try {
+        
+//     } catch (error) {
+//         res.status(error.code || 500 ).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
+
